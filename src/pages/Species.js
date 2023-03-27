f0d0855ff9b0106c745c0549d3245e17c3b31bae
LukaLuxx <luka.jambrosic18@gmail.com>
2023-03-27 15:53:21 +0200

    fourth commit/species done

--- src/pages/Species.js
+++ src/pages/Species.js
@@ -1,24 +1,91 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import LightTheme from "../components/LightTheme";
+import DarkTheme from "../components/DarkTheme";
+import ReactSwitch from "react-switch";
 
 function Species() {
+    const [theme, setTheme] = useState("light");
     const [isOpen, setIsOpen] = useState(false);
+    const [species, setSpecies] = useState([]);
+    const [activeSpecie, setActiveSpecie] = useState(null);
 
-    const toggleDropdown = () => {
+    const names = [
+        "Human",
+        "Droid",
+        "Wookie",
+        "Rodian",
+        "Hutt",
+        "Yoda's species",
+        "Trandoshan",
+        "Mon Calamari",
+        "Ewok",
+        "Sullustan",
+    ];
+
+    useEffect(() => {
+        axios.get("http://swapi.dev/api/species/").then((response) => {
+            setSpecies(response.data.results);
+        });
+    }, []);
+
+    const toggleDropdown = (e) => {
         setIsOpen(!isOpen);
+        setActiveSpecie(
+            activeSpecie === e.target.innerText ? null : e.target.innerText
+        );
     };
+
+    const toggleTheme = () => {
+        setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    };
+
+    const filteredSpecies = activeSpecie
+        ? species.filter((specie) => specie.name === activeSpecie)
+        : [];
+
     return (
-        <div className="dropdown">
-            <button className="dropbtn" onClick={toggleDropdown}>
-                <h1>Name:</h1>
-            </button>
-            {isOpen && (
-                <div className="dropdown-content">
-                    <h2>Classification:</h2>
-                    <h2>Designation:</h2>
-                    <h2>Average height:</h2>
-                    <h2>Skin color:</h2>
+        <div id={theme}>
+            <div className="theme-style">
+                <LightTheme />
+                <ReactSwitch
+                    onChange={toggleTheme}
+                    checked={theme === "dark"}
+                />
+                <DarkTheme />
+            </div>
+            {names.map((name, index) => (
+                <div className="name-container dropbtn" key={index}>
+                    <button className="name-button" onClick={toggleDropdown}>
+                        {name}
+                    </button>
+                    {activeSpecie === name && (
+                        <div className="dropdown">
+                            <div className="dropdown-content">
+                                {filteredSpecies.map((specie) => (
+                                    <div key={specie.name}>
+                                        <div>
+                                            Classification:{" "}
+                                            {specie.classification}
+                                        </div>
+                                        <div>
+                                            Designation:{specie.designation}
+                                        </div>
+                                        <div>
+                                            Average height:
+                                            {specie.average_height}
+                                        </div>
+                                        <div>
+                                            Skin color:{specie.skin_colors}
+                                        </div>
+                                        <hr />
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    )}
                 </div>
-            )}
+            ))}
         </div>
     );
 }
