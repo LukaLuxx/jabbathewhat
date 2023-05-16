3751e6eac5bc2694c26f6950e7db4c80598f9800
LukaLuxx <luka.jambrosic18@gmail.com>
2023-05-16 19:18:35 +0200

    ninth commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import People from "./pages/People";
 import Planets from "./pages/Planets";
 import Species from "./pages/Species";
 import Starships from "./pages/Starships";
+import ThemeProvider from "./components/Theme";
 
 function App() {
     return (
-        <>
+        <ThemeProvider>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/people" element={<People />} />
@@ -17,7 +17,7 @@ function App() {
                 <Route path="/starships" element={<Starships />} />
                 <Route path="/planets" element={<Planets />} />
             </Routes>
-        </>
+        </ThemeProvider>
     );
 }
 
